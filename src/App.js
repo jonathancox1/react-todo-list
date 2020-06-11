d019cc828895cdb681f90d51bf860abb20acef2f
Jonathan Cox <cox.jonathan@gmail.com>
2020-06-11 17:54:51 -0400

    beginnings of to-do list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TodoForm from './components/ToDoForm';
+import { Button, Col, Row } from 'react-bootstrap'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'todo item 1',
+          complete: false,
+        },
+        {
+          name: 'todo item 2',
+          complete: true,
+        },
+      ]
+    }
+  }
+
+  onFormSubmit = (data) => {
+    this.setState({
+      todos: this.state.todos.concat(data)
+    })
+  }
+
+  crossOff = () => {
+
+  }
+
+  handleClick = (e) => {
+    let location = Number(e.target.id);
+    let newTodos = { ...this.state.todos };
+    newTodos[location].complete = !newTodos[location].complete;
+    this.setState({ newTodos })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Row>
+            <Col md={6}>
+              <TodoForm onFormSubmit={this.onFormSubmit} />
+            </Col>
+            <Col md={6}>
+              <ul>
+                {this.state.todos.map((todo, index) => {
+                  return (
+                    <li>{(todo.complete) ? (<strike>{todo.name}</strike>) : (<div>{todo.name}<Button id={index} onClick={this.handleClick}>X</Button></div>)
+                    }</li>
+                  )
+                })
+                }
+              </ul>
+            </Col>
+          </Row>
+        </header>
+      </div >
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/ToDoForm.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { Button, Col, Row, FormControl, InputGroup } from 'react-bootstrap'
+
+export default class TaskInput extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            complete: false,
+        }
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onFormSubmit(this.state);
+        this.setState({
+            name: '',
+            complete: false,
+        })
+    }
+
+    onNameChange = (e) => {
+        this.setState({
+            name: e.target.value,
+        })
+    }
+
+    onCompleteChange = (e) => {
+        this.setState({
+            complete: e.target.checked
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <InputGroup className="mb-3">
+                    <FormControl
+                        placeholder='Add a task'
+                        type="text"
+                        id="name"
+                        value={this.state.name}
+                        onChange={this.onNameChange}
+                    />
+                    <InputGroup.Append>
+                        <Button variant="outline-secondary" type="submit">Add</Button>
+                    </InputGroup.Append>
+                </InputGroup>
+                <input type="checkbox" value="complete" checked={this.state.complete} onChange={this.onCompleteChange} />
+                <label htmlfor="complete"><small>Complete</small></label>
+
+            </form >
+        )
+    }
+}
+
+
