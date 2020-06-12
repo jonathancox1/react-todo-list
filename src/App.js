e1ee7ba3bb4aea92c69b5c449c53167a37ca637a
Jonathan Cox <cox.jonathan@gmail.com>
2020-06-12 08:55:00 -0400

    updates deleteItem function"

--- src/App.js
+++ src/App.js
@@ -28,22 +28,14 @@ class App extends React.Component {
     })
   }
 
-  crossOff = () => {
-
-  }
-
-  handleClick = (e) => {
-    let index = Number(e.target.id);
+  crossOff = (index) => {
     let newTodos = { ...this.state.todos };
     newTodos[index].complete = !newTodos[index].complete;
     this.setState({ newTodos })
   }
 
-  deleteItem = (e) => {
-    let index = Number(e.target.id);
-    // let newTodos = { ...this.state.todos };
-    // console.log(newTodos[index])
-    let newTodos = this.state.todos.filter((todo => todo.name !== this.state.todos[index].name));
+  deleteItem = (index) => {
+    let newTodos = this.state.todos.filter((todo => todo !== this.state.todos[index]));
     console.log(newTodos);
     this.setState({ todos: newTodos, newTodos: newTodos })
   }
@@ -65,7 +57,7 @@ class App extends React.Component {
                 {this.state.todos.map((todo, index) => {
                   return (
                     <div className="ml-3">{(todo.complete) ? (undefined) :
-                      (<div><button className="btn btn-outline-success btn-sm mr-2" id={index} onClick={this.handleClick}>✔️</button><button className="btn btn-outline-danger btn-sm mr-2" id={index} onClick={this.deleteItem}>❌</button>{todo.name}</div>)
+                      (<div><button className="btn btn-outline-success btn-sm mr-2" id={index} onClick={this.crossOff.bind(this, index)}>✔️</button><button className="btn btn-outline-danger btn-sm mr-2" id={index} onClick={this.deleteItem.bind(this, index)}>❌</button>{todo.name}</div>)
                     }</div>
                   )
                 })
@@ -76,7 +68,7 @@ class App extends React.Component {
                 <hr />
                 {this.state.todos.map((todo, index) => {
                   return (
-                    <div className="ml-3">{(todo.complete) ? (<><button className="btn btn-outline-secondary btn-sm mr-2" id={index} onClick={this.handleClick}>⬅️</button><button className="btn btn-outline-danger btn-sm mr-2" id={index} onClick={this.deleteItem}>❌</button><strike>{todo.name}</strike></>) :
+                    <div className="ml-3">{(todo.complete) ? (<><button className="btn btn-outline-secondary btn-sm mr-2" id={index} onClick={this.crossOff.bind(this, index)}>⬅️</button><button className="btn btn-outline-danger btn-sm mr-2" id={index} onClick={this.deleteItem.bind(this, index)}>❌</button><strike>{todo.name}</strike></>) :
                       (undefined)
                     }</div>
                   )
